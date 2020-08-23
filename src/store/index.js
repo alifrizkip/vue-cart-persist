5c673f5dbaa78e20ae8679fa9fbf449fb3304b47
alip <alifrizkipambudi@gmail.com>
2020-08-23 19:14:16 +0700

    persistent state with browser cookies

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
+import Cookies from 'js-cookie';
 
 import cart from './modules/cart';
 
 Vue.use(Vuex);
 
+const cartState = createPersistedState({
+  key: 'cart-persist-state',
+  paths: ['cart.carts'],
+  storage: {
+    getItem: key => Cookies.get(key),
+    setItem: (key, value) => Cookies.set(key, value, { expires: 3, secure: false }),
+    removeItem: key => Cookies.remove(key),
+  },
+});
+
 const store = new Vuex.Store({
   modules: {
     cart,
   },
+  plugins: [cartState],
 });
 
 export default store;

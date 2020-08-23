7b8136fe853e2150b21338fccbc682ec2e8ee5b9
alip <alifrizkipambudi@gmail.com>
2020-08-23 19:11:03 +0700

    persistent state at localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 import cart from './modules/cart';
 
 Vue.use(Vuex);
 
+const cartState = createPersistedState({
+  key: 'cart-persist-state',
+  paths: ['cart.carts'],
+});
+
 const store = new Vuex.Store({
   modules: {
     cart,
   },
+  plugins: [cartState],
 });
 
 export default store;
